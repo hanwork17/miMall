bf80dc42eaab0052dfba8319fa06d2019f9496e4
hanchuanqi <hanwork17@163.com>
2020-04-15 16:44:37 +0800

    mock

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,16 @@
 <script>
 export default {
   name: 'App',
-  components: {
-
+  data(){
+    return {
+      res:[]
+    }
+  },
+  mounted() {
+    this.axios.get('/user/login')
+    .then((res)=>{
+      this.res = res
+    })
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import env from './env'
+// import env from './env'
 
+const mock = true;
+if(mock){
+  require('./mock/api');
+}
+// 根据前端跨域方式调整
+axios.defaults.baseURL = '/api';
 // 根据环境变量获取不同请求地址
-axios.defaults.baseURL = env.baseURL;
+// axios.defaults.baseURL = env.baseURL;
 // 超时8秒
 axios.defaults.timeout = 8000;
 // 接口错误拦截器
