a63e19795aaa33ba99d5e1277a47a0963bf7cc4e
hanchuanqi <hanwork17@163.com>
2020-04-15 23:03:32 +0800

    NavHeader

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data(){
+  name: "App",
+  data() {
     return {
-      res:[]
-    }
+      res: []
+    };
   },
-  mounted() {
-    this.axios.get('/user/login')
-    .then((res)=>{
-      this.res = res
-    })
-  }
-}
+  mounted() {}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import "./assets/scss/reset.scss";
 </style>
