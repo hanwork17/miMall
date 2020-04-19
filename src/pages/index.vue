b51d57795f09ec7458f295582c558f9cfc2d2786
hanchuanqi <hanwork17@163.com>
2020-04-19 23:44:43 +0800

    ServiceBar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-      index
+    <service-bar></service-bar>
   </div>
 </template>
 
 <script>
+import ServiceBar from "./../components/ServiceBar";
 export default {
-  name: 'index'
-}
+  name: "index",
+  components: {
+    ServiceBar
+  }
+};
 </script>
 
 <style>
