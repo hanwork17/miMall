22cadb5a5f2a42dee8ad9f13bb766125fa6a78ab
hanchuanqi <hanwork17@163.com>
2020-04-22 18:04:42 +0800

    首页ads+banner

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,10 @@
               <div class="chlidren">
                 <ul v-for="(item,index) in menuList" :key="index">
                   <li v-for="(sub,j) in item" :key="j">
-                    <a :href="sub?'/#/prooduct/'+sub.id:''"><img :src="sub?sub.img:'/imgs/item-box-1.png'" alt="">{{sub?sub.name:'小米9'}}</a>
+                    <a :href="sub?'/#/prooduct/'+sub.id:''">
+                      <img :src="sub?sub.img:'/imgs/item-box-1.png'" alt />
+                      {{sub?sub.name:'小米9'}}
+                    </a>
                   </li>
                 </ul>
               </div>
@@ -40,15 +43,25 @@
         </div>
         <swiper :options="swiperOptions">
           <swiper-slide v-for="(item,index) in slideList" :key="index">
-            <a :href="'/#/product/'+item.id"><img :src="item.img" alt=""></a>
+            <a :href="'/#/product/'+item.id">
+              <img :src="item.img" alt />
+            </a>
           </swiper-slide>
           <div class="swiper-pagination" slot="pagination"></div>
           <div class="swiper-button-prev" slot="button-prev"></div>
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+        <a :href="'/#/product/30'">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -57,8 +70,8 @@
 
 <script>
 import ServiceBar from "./../components/ServiceBar";
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
-import 'swiper/css/swiper.css';
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 export default {
   name: "index",
   components: {
@@ -66,149 +79,193 @@ export default {
     SwiperSlide,
     ServiceBar
   },
-  data(){
-    return{
-      swiperOptions:{
+  data() {
+    return {
+      swiperOptions: {
         autoplay: true,
         loop: true,
-        effect: 'cube',
+        effect: "cube",
         cubeEffect: {
           shadowOffset: 100,
           shadowScale: 0.6
         },
         pagination: {
-          el: '.swiper-pagination',
+          el: ".swiper-pagination",
           clickable: true
         },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
       },
-      slideList:[{
-        id:'42',
-        img:'/imgs/slider/slide-1.jpg'
-      },{
-        id:'45',
-        img:'/imgs/slider/slide-2.jpg'
-      },{
-        id:'46',
-        img:'/imgs/slider/slide-3.jpg'
-      },{
-        id:'',
-        img:'/imgs/slider/slide-4.jpg'
-      },{
-        id:'',
-        img:'/imgs/slider/slide-5.jpg'
-      },],
-      menuList:[
+      slideList: [
+        {
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg"
+        },
+        {
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg"
+        },
+        {
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg"
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-4.jpg"
+        },
+        {
+          id: "",
+          img: "/imgs/slider/slide-5.jpg"
+        }
+      ],
+      menuList: [
         [
           {
-            id:'30',
-            img:'/imgs/item-box-1.png',
-            name:'小米CC9'
-          },{
-            id:'31',
-            img:'/imgs/item-box-2.png',
-            name:'小米8青春版'
-          },{
-            id:'32',
-            img:'/imgs/item-box-3.jpg',
-            name:'Redmi K20 Pro'
-          },{
-            id:'33',
-            img:'/imgs/item-box-4.jpg',
-            name:'移动4G专区'
+            id: "30",
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9"
+          },
+          {
+            id: "31",
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版"
+          },
+          {
+            id: "32",
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro"
+          },
+          {
+            id: "33",
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区"
           }
-        ],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      adsList: [
+        {
+          id: 33,
+          img: "/imgs/ads/ads-1.png"
+        },
+        {
+          id: 48,
+          img: "/imgs/ads/ads-2.jpg"
+        },
+        {
+          id: 45,
+          img: "/imgs/ads/ads-3.png"
+        },
+        {
+          id: 47,
+          img: "/imgs/ads/ads-4.jpg"
+        }
       ]
-    }
+    };
   }
 };
 </script>
 
 <style lang="scss">
-@import './../assets/scss/config.scss';
-@import './../assets/scss/mixin.scss';
-  .index{
-    .swiper-box{
-      .nav-menu{
-        position: absolute;
-        width: 264px;
-        height: 451px;
-        z-index: 9;
-        padding: 26px 0;
-        background-color: #55585a7a;
-        box-sizing: border-box;
-        .menu-wrap{
-          .menu-item{
-            height: 50px;
-            line-height: 50px;
-            a{
-              font-size: 16px;
-              color: #ffffff;
-              padding-left: 30px;
-              position: relative;
-              display: block;
-              &:after{
-                position: absolute;
-                right: 30px;
-                top: 17.5px;
-                content: '';
-                @include bgImg(10px,15px,'/imgs/icon-arrow.png');
-              }
-            }
-            &:hover{
-              background-color: $colorA;
-              .chlidren{
-                display: block;
-              }
-            }
-            .chlidren{
-              display: none;
-              box-sizing: border-box;
-              width: 962px;
-              height: 451px;
-              background-color: $colorG;
+@import "./../assets/scss/config.scss";
+@import "./../assets/scss/mixin.scss";
+.index {
+  .swiper-box {
+    .nav-menu {
+      position: absolute;
+      width: 264px;
+      height: 451px;
+      z-index: 9;
+      padding: 26px 0;
+      background-color: #55585a7a;
+      box-sizing: border-box;
+      .menu-wrap {
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+          a {
+            font-size: 16px;
+            color: #ffffff;
+            padding-left: 30px;
+            position: relative;
+            display: block;
+            &:after {
               position: absolute;
-              top: 0;
-              left: 264px;
-              border: 1px solid $colorH;
-              ul{
-                display: flex;
-                justify-content: space-between;
+              right: 30px;
+              top: 17.5px;
+              content: "";
+              @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
+            }
+          }
+          &:hover {
+            background-color: $colorA;
+            .chlidren {
+              display: block;
+            }
+          }
+          .chlidren {
+            display: none;
+            box-sizing: border-box;
+            width: 962px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid $colorH;
+            ul {
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li {
                 height: 75px;
-                li{
-                  height: 75px;
-                  line-height: 75px;
-                  flex: 1;
-                  padding-left: 23px;
-                }
-                img{
-                  width: 42px;
-                  height: 35px;
-                  vertical-align: middle;
-                  margin-right: 15px;
-                }
-                a{
-                  color: $colorB;
-                  font-size: 14px;
-                }
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
+              }
+              img {
+                width: 42px;
+                height: 35px;
+                vertical-align: middle;
+                margin-right: 15px;
+              }
+              a {
+                color: $colorB;
+                font-size: 14px;
               }
             }
           }
         }
       }
-      .swiper-container{
-        height: 451px;
-        img{
-          width: 100%;
-          height: 100%;
-        }
-        .swiper-button-prev{
-          left: 274px;
-        }
+    }
+    .swiper-container {
+      height: 451px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      .swiper-button-prev {
+        left: 274px;
       }
     }
   }
+  .ads-box{
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    a{
+      width: 296px;
+      height: 167px;
+    }
+  }
+  .banner{
+    margin-bottom: 50px;
+  }
+}
 </style>
